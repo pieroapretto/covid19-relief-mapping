6ea96c20bead8fa01bfcbe5249f52685a9529349
Piero Pretto <ppretto@amplymedia.com>
2020-03-29 18:21:56 -0500

    update labels

--- src/components/panels/home_panel/WelcomePanel.js
+++ src/components/panels/home_panel/WelcomePanel.js
@@ -7,14 +7,14 @@ import { Link } from 'react-router-dom';
 
 const WelcomePanel = () => (
     <div id="welcome_panel_content">
-        <h1>Help Your Neighbors In Quarantine</h1>
+        <h1>Help Your Neighbors Affected By COVID-19</h1>
         <div className="block-text-container">
             <p>Many people have been negatively impacted by COVID-19. If you have food, household goods, services or time to make a non-contact delivery, sign up to <a href="https://forms.gle/fwSfqDd87EkQFHn78" target="_blank">donate</a>. If you are a member of the vunerable population or need assistance, sign up to <a href="https://forms.gle/cNHiYW5ep9Qx4r1x7" target="_blank">request aid</a>.</p>
         </div>
         <SubHeader Text="Here's How It Works"/>
         <IconRow IconType="donation" Text="Post a donation for non-contact pickup in your area."/>
         <IconRow IconType="map-marked" Text="Avoid stores by looking for donations in your proximity."/>
-        <IconRow IconType="filter" Text="Filter the map based on zipcode, promixity, date and donation type."/>
+        <IconRow IconType="filter" Text="Filter donations and services by zipcode, promixity, date and type."/>
         <div className="btn-container">
             <a href="https://docs.google.com/forms/d/e/1FAIpQLScWAksnaPL5l744YVroZl1MwkOc7htodFq2HPIrSfVYaIjyyw/viewform?usp=sf_link" target="_blank">
                 <ActionButton Text="Offer Help" Class="trinary"/>
