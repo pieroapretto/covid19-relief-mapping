a7291748a6dfd0fb594d73c8582a828521b7f170
Piero Pretto <ppretto@amplymedia.com>
2020-03-30 00:14:33 -0500

    fix bug with search filter

--- src/selectors/locations.js
+++ src/selectors/locations.js
@@ -5,7 +5,7 @@ export const selectProjectLocations = (locations, { types, range, lat_lng, start
 
     return filtered_locations.filter((location) => {
       // search text filter value
-      const searchTextMatch = !searchText || location.description.toLowerCase().includes(searchText) || location.toLowerCase().name.includes(searchText);
+      const searchTextMatch = !searchText || location.name.toLowerCase().includes(searchText) || location.description.toLowerCase().includes(searchText);
 
       //DateRangeSlider value
       const startDateMatch = typeof startDate !== 'number' || location.timestamp >= startDate;
@@ -15,7 +15,6 @@ export const selectProjectLocations = (locations, { types, range, lat_lng, start
       const locationTypeMatch = !types.length || types.includes(location.type);
 
       //Proximity value
-      console.log(lat_lng);
       const location_lat_lng = { lat: location.lat, lng: location.lng };
       const withinProximityMatch = typeof range !== 'number' || withinProximity(lat_lng, location_lat_lng, range);
       
