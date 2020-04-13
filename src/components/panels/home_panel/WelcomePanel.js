53b2d38910910a818b939801c30da009b2ef586b
Piero Pretto <ppretto@amplymedia.com>
2020-04-13 11:48:05 -0500

    wouldnt be my app if theres no typos

--- src/components/panels/home_panel/WelcomePanel.js
+++ src/components/panels/home_panel/WelcomePanel.js
@@ -8,7 +8,7 @@ const WelcomePanel = () => (
     <div id="welcome_panel_content">
         <h1>Help Your Neighbors Affected By COVID-19</h1>
         <div className="block-text-container">
-            <p><a href="https://forms.gle/fwSfqDd87EkQFHn78" target="_blank">Sign up</a> to help your neighbors in needs. If you are activately involved in COVID-19 relief efforts, recruit volunteers or financial assistance to <a href="https://forms.gle/hY6FHftouNUkiuyU8" target="_blank">aid your cause</a>.</p>
+            <p><a href="https://forms.gle/fwSfqDd87EkQFHn78" target="_blank">Sign up</a> to help your neighbors in need. If you are activately involved in COVID-19 relief efforts, recruit volunteers or financial assistance to <a href="https://forms.gle/hY6FHftouNUkiuyU8" target="_blank">aid your cause</a>.</p>
         </div>
         <SubHeader Text="Here's How It Works"/>
         <IconRow IconType="volunteer" Text="Volunteer to help based on your skill set or what you can give."/>
