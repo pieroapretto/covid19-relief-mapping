58d64f91c425289f4a2afe2df28c66931de3d8bf
Piero Pretto <ppretto@amplymedia.com>
2020-04-25 21:26:39 -0500

    add firebase analytics

--- private/firebase.js
+++ private/firebase.js
@@ -5,5 +5,6 @@ export const firebaseConfig = {
     projectId: "covid19-app-dcdf4",
     storageBucket: "covid19-app-dcdf4.appspot.com",
     messagingSenderId: "215438822880",
-    appId: "1:215438822880:web:fec4afb9df4818f4dc6f85"
+    appId: "1:215438822880:web:fec4afb9df4818f4dc6f85",
+    measurementId: "G-06EH1ZE9JZ"
 };
--- src/firebase/firebase_init.js
+++ src/firebase/firebase_init.js
@@ -1,8 +1,11 @@
 import * as firebase from 'firebase/app';
-import 'firebase/database';
 import { firebaseConfig } from '../../private/firebase';
+
+import 'firebase/database';
+import 'firebase/analytics';
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 export const db = firebase.database();
