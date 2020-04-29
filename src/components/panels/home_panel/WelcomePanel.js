26cf8b1c48bcaf7dbf7b4b45d8552c712105bd21
Piero Pretto <ppretto@amplymedia.com>
2020-04-28 22:55:52 -0500

    replace volunteer button with request

--- src/components/panels/home_panel/WelcomePanel.js
+++ src/components/panels/home_panel/WelcomePanel.js
@@ -15,15 +15,15 @@ const WelcomePanel = (props) => {
         <div id="welcome_panel_content">
             <h1>Help Your Neighbors During This Crisis</h1>
             <div className="block-text-container">
-                <p><a href="https://forms.gle/fwSfqDd87EkQFHn78" target="_blank">Sign up</a> to help your neighbors in need. If you are activately involved in COVID-19 relief efforts, recruit volunteers or financial assistance to <a href="https://forms.gle/hY6FHftouNUkiuyU8" target="_blank">aid your cause</a>.</p>
+                <p><a href="https://forms.gle/fwSfqDd87EkQFHn78" target="_blank">Sign up</a> to help your neighbors in need. If you are activately involved in COVID-19 relief efforts, recruit volunteers or financial assistance to <a href="https://forms.gle/PuchtZrmHwCJ1GNs9" target="_blank">aid your cause</a>.</p>
             </div>
             <SubHeader Text="Here's How It Works"/>
             <IconRow IconType="volunteer" Text="Volunteer to help based on your skill set or what you can give."/>
             <IconRow IconType="donation" Text="Post a donation for non-contact pickup or delivery in your area."/>
             <IconRow IconType="filter" Text="Search for help in your area by zipcode, promixity, date and type."/>
             <div className="btn-container">
-                <a href="https://forms.gle/BzRNsaxVkRodG4vPA" target="_blank">
-                    <ActionButton Text="Volunteer" Class="trinary-large"/>
+                <a href="https://forms.gle/PuchtZrmHwCJ1GNs9" target="_blank">
+                    <ActionButton Text="Request Help" Class="trinary-large"/>
                 </a>
                 <a href="https://forms.gle/s8CkMbssBupfsvxVA" target="_blank">
                     <ActionButton Text="Donate" Class="primary-large"/>
