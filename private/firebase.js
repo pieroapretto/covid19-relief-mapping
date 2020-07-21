b7c8e722a1b9542d89e86be1f483f505db8b54aa
Piero Pretto <ppretto@amplymedia.com>
2020-07-20 23:56:08 -0500

    hardcode measurement id

--- private/firebase.js
+++ private/firebase.js
@@ -1,3 +1,5 @@
+console.log(process.env.FIREBASE_MESSAGING_SENDER_ID);
+
 export const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -6,5 +8,5 @@ export const firebaseConfig = {
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    measurementId: 'G-0GY93DVBK5'
 };
